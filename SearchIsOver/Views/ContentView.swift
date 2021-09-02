75dfbeb722403b1a96ff3f497f1c4760e761b21f
Vui Nguyen <vui_engineer@yahoo.com>
2021-09-02 15:20:00 -0600

    more ice cream flavors & filters

--- SearchIsOver/Views/ContentView.swift
+++ SearchIsOver/Views/ContentView.swift
@@ -13,13 +13,27 @@ struct ContentView: View {
            // .padding()
         VStack(alignment: .leading) {
             CheckboxView(isChecked: false, title: "Chocolate")
+            CheckboxView(isChecked: false, title: "Coffee")
             CheckboxView(isChecked: false, title: "Fruit")
+            CheckboxView(isChecked: false, title: "None of Above")
             List {
+                Group {
                 Text("Vanilla Ice Cream")
                 Text("Chocolate Ice Cream")
                 Text("Strawberry Ice Cream")
+                Text("Blueberry Sorbert")
+                Text("Cherry Garcia")
+                Text("Mint Chocolate Chip")
+                }
+                Group {
+                Text("Chocolate Covered Strawberries")
+                Text("Coffee Espresso")
+                Text("Chocolate Coffee Espresso")
+                Text("Chocolate Raspberry Coffee Espresso")
+                Text("Vanilla Bean Coffee Espresso")
+                }
             }
-        }
+        } .padding()
     }
 }
 

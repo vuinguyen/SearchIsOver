f81f4602cabc925c955e5a0924c36c0a5c701901
Vui Nguyen <vui_engineer@yahoo.com>
2021-09-02 08:23:18 -0600

    Initial Commit

---
+++ SearchIsOver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchIsOver
+//
+//  Created by Vui Nguyen on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchIsOver/SearchIsOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchIsOverApp.swift
+//  SearchIsOver
+//
+//  Created by Vui Nguyen on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SearchIsOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
